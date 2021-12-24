5c29d02dd2504f1f5a78ce8cf7077c91c1555eaf
Eduardo Nascimento <92709734+edrhat@users.noreply.github.com>
2021-12-24 00:52:40 -0300

    Add files via upload

--- dir.sh
+++ dir.sh
@@ -2,7 +2,7 @@
 
 for p in $(cat $2)
 do
-r=$(curl -s -o /dev/null -w "%{http_code}" http://$1/$p/)
+r=$(curl -s -o /dev/null -w "%{http_code}" -A "Wh04m1" http://$1/$p/)
 if [ $r == "200" ]
 then
 echo "Diretório encontrado: http://businessocorp.com.br/$p"
